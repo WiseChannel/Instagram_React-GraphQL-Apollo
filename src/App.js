ad53ddb50505e8848589722fafddcdd744761d41
Ilya <ilyaphotoshop17@gmail.com>
2020-04-14 03:15:50 +0300

    [fix] change gitignore

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React, { useState, useEffect, ReactFragment } from 'react';
-
-import Header from './Components/Header';
-import Login from './Components/Login';
-import CreatePost from './Components/CreatePost'
-
-const App = () => {
-  const [user, setUser] = useState('Ilya')
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    document.title = user ? `${user}'s Feed` : 'Please login'
-  }, [user])
-
-  if (!user) return <Login setUser={setUser} />
-
-  return (
-    <>
-      <Header 
-        user={user} 
-        setUser={setUser}
-      />
-      <CreatePost 
-        user={user} 
-        setPosts={setPosts}
-        posts={posts}
-      />
-      {posts.map((post, i) => (
-        <React.Fragment key={i}>
-          {post.image && (
-            <img 
-              style={{ height: 100, width: 100, objectFit: 'cover' }}
-              src={URL.createObjectURL(post.image)}
-              alt='Post Cover'
-            />
-          )}
-          <p>{post.content}</p>
-          <div>{user}</div>
-        </React.Fragment>
-      ))}
-    </>
-  );
-}
-
-export default App
+// import React, { useState, useEffect } from 'react';
+//
+// import Header from './Components/Header';
+// import Login from './Components/Login';
+// import CreatePost from './Components/CreatePost'
+//
+// const App = () => {
+//
+//   const [user, setUser] = useState('Ilya')
+//   const [posts, setPosts] = useState([])
+//
+//   useEffect(() => {
+//     document.title = user ? `${user}'s Feed` : 'Please login'
+//   }, [user])
+//
+//   if (!user) return <Login setUser={setUser} />
+//
+//   return (
+//     <>
+//       <Header
+//         user={user}
+//         setUser={setUser}
+//       />
+//       <CreatePost
+//         user={user}
+//         setPosts={setPosts}
+//         posts={posts}
+//       />
+//       {posts.map((post, i) => (
+//         <React.Fragment key={i}>
+//           {post.image && (
+//             <img
+//               style={{ height: 100, width: 100, objectFit: 'cover' }}
+//               src={URL.createObjectURL(post.image)}
+//               alt='Post Cover'
+//             />
+//           )}
+//           <p>{post.content}</p>
+//           <div>{user}</div>
+//         </React.Fragment>
+//       ))}
+//     </>
+//   );
+// }
+//
+// export default App
+//
+//
